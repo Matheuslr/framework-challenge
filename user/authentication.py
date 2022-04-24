f690f08ac6c2b46540f8a9537030b8ecbeb31204
Matheus Reis <matheuslr27@gmail.com>
2022-04-24 15:04:57 -0300

    style: :loud_sound: add errors logs (#3)

--- user/authentication.py
+++ user/authentication.py
@@ -3,6 +3,7 @@ from django.conf import settings
 from rest_framework.authentication import BaseAuthentication
 from rest_framework.exceptions import AuthenticationFailed
 
+from framework_api.helpers import error_logging
 from user.models import User
 
 
@@ -17,6 +18,7 @@ class CustomUserAuthentication(BaseAuthentication):
                 token, settings.JWT_SECRET, algorithms=["HS256"]
             )  # no qa
         except Exception:
+            error_logging(data="Unathorized user", status_code=401)
             raise AuthenticationFailed("Unathorized")
 
         user = User.objects.filter(id=payload["id"]).first()
