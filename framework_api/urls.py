df566e22ba0e8a05f0a51ed8e7ea63c4be8bd3b1
matheuslr <matheuslr27@gmail.com>
2022-04-24 14:47:49 -0300

    docs: :hammer: inproved docs and add OpenAPI

--- framework_api/urls.py
+++ framework_api/urls.py
@@ -43,5 +43,10 @@ urlpatterns = [
         schema_view.with_ui("swagger", cache_timeout=0),
         name="schema-swagger-ui",
     ),
+    path(
+        "redoc/",
+        schema_view.with_ui("redoc", cache_timeout=0),
+        name="schema-redoc",  # no qa
+    ),  # no qa
 ]
 urlpatterns += router.urls
